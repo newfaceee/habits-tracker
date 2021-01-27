7c4d17bf6640821d45249c6301555c7422178fee
Edem <50662754+newfaceee@users.noreply.github.com>
2021-01-27 14:49:15 +0300

    Подключение шрифта Jost

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import App from "./App";
+import theme from "./shared";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
